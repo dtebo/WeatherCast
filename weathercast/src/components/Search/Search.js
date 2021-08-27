540bb10272dbb5a603ef1c2441012a48f41a12ae
dtebo <darrentebo83@outlook.com>
2021-08-27 09:54:37 -0400

    added search input field

--- weathercast/src/components/Search/Search.js
+++ weathercast/src/components/Search/Search.js
@@ -1,8 +1,23 @@
 import react from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+        },
+    },
+}));
 
 const Search = () => {
+    const classes = useStyles();
+
     return (
-        <h1>Search Goes Here...</h1>
+        <form className={classes.root}>
+            <TextField id="outlined-basic" label="Search by Zip" variant="outlined" />
+        </form>
     );
 };
 
