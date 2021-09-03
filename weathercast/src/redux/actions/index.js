b032e8ec8f678d62bc3e5cbb4a2948d214cb4385
dtebo <darrentebo83@outlook.com>
2021-09-03 16:09:13 -0400

    implemented getWeatherData action creator

--- weathercast/src/redux/actions/index.js
+++ weathercast/src/redux/actions/index.js
@@ -1,2 +1,5 @@
-import thunk from 'redux-thunk';
+export const FETCH_WEATHER_DATA_START = 'FETCH_WEATHER_DATA_START';
 
+export const getWeatherData = () => dispatch => {
+    dispatch({type: FETCH_WEATHER_DATA_START});
+};
